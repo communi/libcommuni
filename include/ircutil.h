d41469793ae1115f29ebb38b5ee7a526d18cfad1
J-P Nurmi <jpnurmi@gmail.com>
2009-09-22 23:27:54 +0300

    Removed obsolete svn id keywords.

--- include/ircutil.h
+++ include/ircutil.h
@@ -10,8 +10,6 @@
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 * License for more details.
-*
-* $Id$
 */
 
 #ifndef IRC_UTIL_H
