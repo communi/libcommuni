ee5ec56fbd0c9eb9b68714a31a842f234fec85e7
J-P Nurmi <jpnurmi@gmail.com>
2011-10-02 22:22:15 +0200

    RPL_BOUNCE -> RPL_ISUPPORT

--- include/irc.h
+++ include/irc.h
@@ -32,7 +32,7 @@ public:
         RPL_YOURHOST               = 002,
         RPL_CREATED                = 003,
         RPL_MYINFO                 = 004,
-        RPL_BOUNCE                 = 005,
+        RPL_ISUPPORT               = 005,
 
         RPL_USERHOST               = 302,
         RPL_ISON                   = 303,
