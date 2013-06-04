1fbdb5dee754a06aa5ced0d26681049e83481337
J-P Nurmi <jpnurmi@gmail.com>
2013-06-04 14:49:34 +0200

    Fix IrcCommandParser auto test

--- tests/auto/irccommandparser/tst_irccommandparser.cpp
+++ tests/auto/irccommandparser/tst_irccommandparser.cpp
@@ -40,35 +40,35 @@ void tst_IrcCommandParser::testParse_data()
     QTest::addColumn<QString>("input");
     QTest::addColumn<QString>("output");
 
-    QTest::newRow("join1") << QString("#communi") << QString("JOIN") << QString();
-    QTest::newRow("join2") << QString("#communi") << QString("JOIN #chan") << QString("JOIN #chan");
-    QTest::newRow("join3") << QString("#communi") << QString("JOIN #chan secret") << QString("JOIN #chan secret");
-    QTest::newRow("join4") << QString("#communi") << QString("JOIN #chan too secret") << QString();
+    QTest::newRow("join1") << QString("#communi") << QString("/JOIN") << QString();
+    QTest::newRow("join2") << QString("#communi") << QString("/JOIN #chan") << QString("JOIN #chan");
+    QTest::newRow("join3") << QString("#communi") << QString("/JOIN #chan secret") << QString("JOIN #chan secret");
+    QTest::newRow("join4") << QString("#communi") << QString("/JOIN #chan too secret") << QString();
 
-    QTest::newRow("part1") << QString("#communi") << QString("PART") << QString("PART #communi");
-    QTest::newRow("part2") << QString("#communi") << QString("PART #communi") << QString("PART #communi");
-    QTest::newRow("part3") << QString("#communi") << QString("PART #not-exist") << QString("PART #communi :#not-exist");
-    QTest::newRow("part4") << QString("#communi") << QString("PART hasta la vista") << QString("PART #communi :hasta la vista");
-    QTest::newRow("part5") << QString("#communi") << QString("PART #chan hasta la vista") << QString("PART #communi :#chan hasta la vista");
+    QTest::newRow("part1") << QString("#communi") << QString("/PART") << QString("PART #communi");
+    QTest::newRow("part2") << QString("#communi") << QString("/PART #communi") << QString("PART #communi");
+    QTest::newRow("part3") << QString("#communi") << QString("/PART #not-exist") << QString("PART #communi :#not-exist");
+    QTest::newRow("part4") << QString("#communi") << QString("/PART hasta la vista") << QString("PART #communi :hasta la vista");
+    QTest::newRow("part5") << QString("#communi") << QString("/PART #chan hasta la vista") << QString("PART #communi :#chan hasta la vista");
 
-    QTest::newRow("kick1") << QString("#communi") << QString("KICK") << QString();
-    QTest::newRow("kick2") << QString("#communi") << QString("KICK #communi") << QString();
-    QTest::newRow("kick3") << QString("#communi") << QString("KICK jpnurmi") << QString("KICK #communi jpnurmi");
-    QTest::newRow("kick4") << QString("jpnurmi")  << QString("KICK jpnurmi") << QString();
-    QTest::newRow("kick5") << QString("#communi") << QString("KICK #communi jpnurmi") << QString("KICK #communi jpnurmi");
-    QTest::newRow("kick6") << QString("jpnurmi")  << QString("KICK jpnurmi jpnurmi") << QString();
-    QTest::newRow("kick7") << QString("#communi") << QString("KICK #communi jpnurmi hasta la vista") << QString("KICK #communi jpnurmi :hasta la vista");
-    QTest::newRow("kick8") << QString("jpnurmi")  << QString("KICK jpnurmi jpnurmi hasta la vista") << QString();
-    QTest::newRow("kick9") << QString("#communi") << QString("KICK jpnurmi hasta la vista") << QString("KICK #communi jpnurmi :hasta la vista");
+    QTest::newRow("kick1") << QString("#communi") << QString("/KICK") << QString();
+    QTest::newRow("kick2") << QString("#communi") << QString("/KICK #communi") << QString();
+    QTest::newRow("kick3") << QString("#communi") << QString("/KICK jpnurmi") << QString("KICK #communi jpnurmi");
+    QTest::newRow("kick4") << QString("jpnurmi")  << QString("/KICK jpnurmi") << QString();
+    QTest::newRow("kick5") << QString("#communi") << QString("/KICK #communi jpnurmi") << QString("KICK #communi jpnurmi");
+    QTest::newRow("kick6") << QString("jpnurmi")  << QString("/KICK jpnurmi jpnurmi") << QString();
+    QTest::newRow("kick7") << QString("#communi") << QString("/KICK #communi jpnurmi hasta la vista") << QString("KICK #communi jpnurmi :hasta la vista");
+    QTest::newRow("kick8") << QString("jpnurmi")  << QString("/KICK jpnurmi jpnurmi hasta la vista") << QString();
+    QTest::newRow("kick9") << QString("#communi") << QString("/KICK jpnurmi hasta la vista") << QString("KICK #communi jpnurmi :hasta la vista");
 
-    QTest::newRow("me1") << QString("jpnurmi")  << QString("ME") << QString();
-    QTest::newRow("me2") << QString("#communi") << QString("ME loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
-    QTest::newRow("me3") << QString("jpnurmi")  << QString("ME loves communi") << QString("PRIVMSG jpnurmi :\1ACTION loves communi\1");
+    QTest::newRow("me1") << QString("jpnurmi")  << QString("/ME") << QString();
+    QTest::newRow("me2") << QString("#communi") << QString("/ME loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
+    QTest::newRow("me3") << QString("jpnurmi")  << QString("/ME loves communi") << QString("PRIVMSG jpnurmi :\1ACTION loves communi\1");
 
-    QTest::newRow("action1") << QString("jpnurmi")  << QString("ACTION") << QString();
-    QTest::newRow("action2") << QString("#communi") << QString("ACTION #communi loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
-    QTest::newRow("action3") << QString("jpnurmi")  << QString("ACTION jpnurmi loves communi") << QString("PRIVMSG jpnurmi :\1ACTION loves communi\1");
-    QTest::newRow("action4") << QString("jpnurmi")  << QString("ACTION #communi loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
+    QTest::newRow("action1") << QString("jpnurmi")  << QString("/ACTION") << QString();
+    QTest::newRow("action2") << QString("#communi") << QString("/ACTION #communi loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
+    QTest::newRow("action3") << QString("jpnurmi")  << QString("/ACTION jpnurmi loves communi") << QString("PRIVMSG jpnurmi :\1ACTION loves communi\1");
+    QTest::newRow("action4") << QString("jpnurmi")  << QString("/ACTION #communi loves communi") << QString("PRIVMSG #communi :\1ACTION loves communi\1");
 }
 
 void tst_IrcCommandParser::testParse()
