d6fd05574b805fe2afea5ff16140e3a24543def3
J-P Nurmi <jpnurmi@gmail.com>
2016-07-24 10:49:18 +0200

    Welcome to 2016

--- tests/auto/irctextformat/tst_irctextformat.cpp
+++ tests/auto/irctextformat/tst_irctextformat.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2015 The Communi Project
+ * Copyright (C) 2008-2016 The Communi Project
  *
  * This test is free, and not covered by the BSD license. There is no
  * restriction applied to their modification, redistribution, using and so on.
