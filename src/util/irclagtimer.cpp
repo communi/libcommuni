communi/libcommuni

commit d3e388de9a146faad3277b46e480b0f1415f9a24

count unanswered pings towards the lag (#59)
if a PONG has not been received and we are about to send a new PING,
that's lag of one interval

src/util/irclagtimer.cpp
@@ -58,7 +58,7 @@ static const int DEFAULT_INTERVAL = 60;
  */
 
 #ifndef IRC_DOXYGEN
-IrcLagTimerPrivate::IrcLagTimerPrivate() : q_ptr(0), connection(0), interval(DEFAULT_INTERVAL), lag(-1)
+IrcLagTimerPrivate::IrcLagTimerPrivate() : q_ptr(0), connection(0), interval(DEFAULT_INTERVAL), pendingPings(0), lag(-1)
 {
 }
 
@@ -77,6 +77,7 @@ bool IrcLagTimerPrivate::processPongReply(IrcPongMessage* msg)
         bool ok = false;
         qint64 timestamp = msg->argument().mid(8).toLongLong(&ok);
         if (ok) {
+            pendingPings--;
             updateLag(QDateTime::currentMSecsSinceEpoch() - timestamp);
             return true;
         }
@@ -88,6 +89,7 @@ bool IrcLagTimerPrivate::processPongReply(IrcPongMessage* msg)
 void IrcLagTimerPrivate::_irc_connected()
 {
 #if QT_VERSION >= 0x040700
+    pendingPings = 0;
     if (interval > 0)
         timer.start();
 #endif // QT_VERSION
@@ -99,6 +101,11 @@ void IrcLagTimerPrivate::_irc_pingServer()
     // TODO: configurable format?
     QString cmd = QString("PING communi/%1").arg(QDateTime::currentMSecsSinceEpoch());
     connection->sendData(cmd.toUtf8());
+    qint64 pingLag = pendingPings * interval * 1000;
+    if (pingLag > lag) {
+        updateLag(pingLag);
+    }
+    pendingPings++;
 #endif // QT_VERSION
 }
 
@@ -106,6 +113,7 @@ void IrcLagTimerPrivate::_irc_disconnected()
 {
 #if QT_VERSION >= 0x040700
     updateLag(-1);
+    pendingPings = 0;
     if (timer.isActive())
         timer.stop();
 #endif // QT_VERSION

include/IrcUtil/irclagtimer_p.h
@@ -60,6 +60,7 @@ public:
     IrcConnection* connection;
     QTimer timer;
     int interval;
+    int pendingPings;
     qint64 lag;
 };
 

commit 3abc2a020385a2e70ffa8e225a41eafc510123cd

Fix IrcLagTimer to pass the auto tests again
Had been broken since d3e388de:

    FAIL!  : tst_IrcLagTimer::testLag() Compared values are not the same
       Actual   (lagSpy.count()): 2
       Expected (++lagCount)    : 1
       Loc: [tst_irclagtimer.cpp(83)]

src/util/irclagtimer.cpp
@@ -77,7 +77,7 @@ bool IrcLagTimerPrivate::processPongReply(IrcPongMessage* msg)
         bool ok = false;
         qint64 timestamp = msg->argument().mid(8).toLongLong(&ok);
         if (ok) {
-            pendingPings--;
+            --pendingPings;
             updateLag(QDateTime::currentMSecsSinceEpoch() - timestamp);
             return true;
         }
@@ -102,10 +102,9 @@ void IrcLagTimerPrivate::_irc_pingServer()
     QString cmd = QString("PING communi/%1").arg(QDateTime::currentMSecsSinceEpoch());
     connection->sendData(cmd.toUtf8());
     qint64 pingLag = pendingPings * interval * 1000;
-    if (pingLag > lag) {
+    if (lag > -1 && pingLag > lag)
         updateLag(pingLag);
-    }
-    pendingPings++;
+    ++pendingPings;
 #endif // QT_VERSION
 }
 
@@ -137,8 +136,9 @@ void IrcLagTimerPrivate::updateTimer()
 void IrcLagTimerPrivate::updateLag(qint64 value)
 {
     Q_Q(IrcLagTimer);
+    value = qMax(-1ll, value);
     if (lag != value) {
-        lag = qMax(-1ll, value);
+        lag = value;
         emit q->lagChanged(lag);
     }
 }