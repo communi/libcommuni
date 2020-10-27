03886f3d1a4df5454240e31da151a88adaa45d69
Stephen Lyons <slysven@virginmedia.com>
2020-10-27 04:16:12 +0000

    Revise: reduce warnings by handling some code that Qt has declared obsolete
    As of Qt 5.15.0 I am finding that some items are producing obsolete or
    deprecated code warnings. Specifically:
    * the `QString::SplitBehaviour` enum has been replaced by a
      `Qt::SplitBehaviour` one (in Qt 5.14)
    * qrand() and qsrand() have been declared obsolete and the former can be
      replaced with methods in a `QRandomGenerator` class. Originally provided
      in Qt 5.10 and declared obsolete in some version after that.
    
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- examples/bot/main.cpp
+++ examples/bot/main.cpp
@@ -8,6 +8,9 @@
  */
 
 #include <QtCore>
+#if (QT_VERSION) >= (QT_VERSION_CHECK(5, 10, 0))
+#include <QRandomGenerator>
+#endif
 #include "ircbot.h"
 
 int main(int argc, char* argv[])
@@ -16,12 +19,16 @@ int main(int argc, char* argv[])
 
     // enable debug output
     qputenv("IRC_DEBUG", "1");
-    qsrand(QTime::currentTime().msec());
 
     IrcBot bot;
     bot.setHost("irc.freenode.net");
     bot.setUserName("communi");
+#if (QT_VERSION) >= (QT_VERSION_CHECK(5, 10, 0))
+    bot.setNickName("Bot" + QString::number(QRandomGenerator::global()->bounded(1, 10000)));
+#else
+    qsrand(QTime::currentTime().msec());
     bot.setNickName("Bot" + QString::number(qrand() % 9999));
+#endif
     bot.setRealName("Communi " + Irc::version() + " example bot");
 
     bool joined = false;
